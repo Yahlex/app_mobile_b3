234e76e8321eea0a41698e1cdd1d8435969816cc
Yahlex <alexisremyh@gmail.com>
2024-04-30 12:37:47 +0200

    :art: add TabBar

--- src/navigation/Navigator.jsx
+++ src/navigation/Navigator.jsx
@@ -3,6 +3,7 @@ import HomeScreen from '../screens/HomeScreen'
 import ProductScreen from '../screens/ProductScreen'
 import ProfileScreen from '../screens/ProfileScreen'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import CustomTabBar from '../navigation/CustomTabBar'
 
 const Stack = createStackNavigator()
 
@@ -18,7 +19,7 @@ const BottomTab = createBottomTabNavigator()
 
 const TabNavigator = () => {
   return (
-    <BottomTab.Navigator>
+    <BottomTab.Navigator tabBar={props => <CustomTabBar {...props} />}>
       <BottomTab.Screen name='HomeStack' options={{ headerShown: false, title: 'Home' }} component={StackNavigator} />
       <BottomTab.Screen name='Product' component={ProductScreen} />
     </BottomTab.Navigator>
